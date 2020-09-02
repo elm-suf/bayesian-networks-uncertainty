2dd0c7c1655bfff844c4b006d41a78730addac02
elm-suf <elm.sufian@gmail.com>
2020-09-02 17:16:46 +0200

    feat(init): 🚀 project structure

---
+++ src/main/java/bnparser/BNParser.java
@@ -0,0 +1,29 @@
+package bnparser;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import aima.core.probability.RandomVariable;
+import aima.core.probability.bayes.BayesianNetwork;
+
+import java.util.List;
+
+/**
+ *
+ * @author torta
+ */
+public class    BNParser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BayesianNetwork bn = BifReader.readBIF("earthquake.xml");
+        List<RandomVariable> rvs = bn.getVariablesInTopologicalOrder();
+        for (RandomVariable rv :rvs) {
+            System.out.println(rv.getName());
+        }
+        }   
+    
+}
--- src/main/java/com/github/elmsuf/App.java
+++ src/main/java/com/github/elmsuf/App.java
@@ -1,7 +1,11 @@
 package com.github.elmsuf;
 
+import aima.core.probability.bayes.BayesianNetwork;
+import bnparser.BifReader;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String... args) {
+        BayesianNetwork bn = BifReader.readBIF("src/main/resources/networks/alarm.xml");
+        System.out.println(bn);
     }
 }
